f5fb551bc6d0fff6a6ce369adfa87adfe33670f7
jmendez44 <jenuel.mendez@gmail.com>
2019-12-20 03:23:25 -0500

    config file changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [
       {
         name: "topicks-api",
-        script: "./src/index.js",
+        script: "./topic-server1/src/index.js",
         watch: true,
         env: {
             "DB_CONNECTION": "mongodb+srv://JAY:1234@testcluster1-kpwsg.mongodb.net/test?retryWrites=true&w=majority"
